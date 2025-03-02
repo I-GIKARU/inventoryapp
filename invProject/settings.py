c53f52681752773f668e172183a7846552cc5a80
I-GIKARU <aizohgikaru@gmail.com>
2025-03-02 19:18:50 +0300

    host

--- invProject/settings.py
+++ invProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q#vq)g3jif&z1r_h8ei*%@4#60tv%39$yak+%u8z3_neq(w$qz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

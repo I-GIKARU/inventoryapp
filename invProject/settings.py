1266bb4b4bc2d17ae3c7f48a9ae578eb7f3c5f80
I-GIKARU <aizohgikaru@gmail.com>
2025-03-02 21:20:01 +0300

    db fixed

--- invProject/settings.py
+++ invProject/settings.py
@@ -94,10 +94,15 @@ DATABASES = {
 }
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'inventorydb_yqfi',
+#         'USER': 'inventorydb_yqfi_user',
+#         'PASSWORD': '1TFEaYVSB1VGePNlBSL93pEx4ucPTg4Y',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
 
 
 # Password validation
